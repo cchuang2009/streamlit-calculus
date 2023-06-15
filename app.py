f6f60e4c0e10481f3265273c7516a1e2c9695326
Chu-Ching Huang <cchuang2009@gmail.com>
2023-06-15 21:18:39 +0800

    exception handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sympy as sp
 from sympy import *
 from sympy.printing.latex import latex
 from io import StringIO
@@ -29,13 +30,22 @@ def main():
     XR = st.text_input("Enter the ranges (comma-separated):","[[0,1],[0,2]]")
 
     if st.button("Calculate"):
-       # Call the MultipleIntegral function
-        X_Sp= sympify(X)
-        XR_Sp= sympify(XR)
-        f_Sp= sympify(f)
-        result=MultipleIntegral_st(f_Sp, X_Sp, XR_Sp)        
-        # only result was read (f) in raw format (r)
-        st.latex(rf'''{result}''')
+        try:
+            # Call the MultipleIntegral function
+            X_Sp = sp.sympify(X)
+            XR_Sp = sp.sympify(XR)
+            f_Sp = sp.sympify(f)
+            result = MultipleIntegral_st(f_Sp, X_Sp, XR_Sp)
+
+            # Display the result in LaTeX format
+            st.latex(rf"{result}")
+
+        except (sp.SympifyError, ValueError, TypeError) as e:
+            st.error("Can't be evaluated!")
+            print(e)
+        except NameError as e:
+            st.error("Can't solve; ask your mentor to help.")
+            print(e)    
 
 if __name__ == '__main__':
     main()
